8e3b68853c8766f78c5c8bd1b1176437285cf042
Tom Fiedler <zenkazio@gmail.com>
2025-04-08 17:25:09 +0200

    completed log file save; next should be the logs and there extensions themselves

--- src/log.rs
+++ src/log.rs
@@ -1,7 +1,40 @@
+use chrono::Local;
+use current_platform::{COMPILED_ON, CURRENT_PLATFORM};
 use serde::{Deserialize, Serialize};
 use serde_json::{from_str, to_string_pretty};
+use std::{fs, path};
 
 #[derive(Serialize, Deserialize)]
-pub struct Log {}
+pub struct Log {
+    name: String,
+    creation_time: String,
+}
 
-impl Log {}
+impl Log {
+    pub fn new() -> Self {
+        let local_time = Local::now();
+
+        Log {
+            name: format!("{}-{CURRENT_PLATFORM}", local_time.format("%Y-%m-%d")),
+            creation_time: local_time.format("%Y-%m-%d %H:%M:%S").to_string(),
+        }
+    }
+
+    pub fn to_file(&self) {
+        let logs_dir = "./logs";
+        let log_file_name = format!("{}_log.json", self.name);
+        match path::Path::new(logs_dir).exists() {
+            true => {}
+            false => match fs::create_dir(logs_dir) {
+                Ok(()) => (),
+                Err(err) => println!("Directory Creation Error: {}", err),
+            },
+        }
+        fs::write(format!("{}/{}", logs_dir, log_file_name), self.to_json()).unwrap();
+
+        println!("{}", self.to_json());
+    }
+    pub fn to_json(&self) -> String {
+        to_string_pretty(&self).expect("Something went wrong turning the Log into Json")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,21 @@
 //#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![allow(dead_code, unused_imports, unused_variables)]
 
-use serde::{Deserialize, Serialize};
-use serde_json::{from_str, to_string_pretty};
+use std::time::{Duration, Instant};
+
+use chrono::{Local, Utc};
+
 mod cli;
 mod client;
+mod log;
 mod server;
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename = "dato")]
-#[serde(rename_all = "camelCase")]
-struct Data {
-    number: u32,
-    string_test: String,
-}
-
-impl Data {}
 
 fn main() {
     // match cli::is_server() {
     //     true => server::server_job(),
     //     false => client::client_job(),
     // }
-    let data = Data {
-        number: 10,
 
-        string_test: String::from("test"),
-    };
-    let json_string: String = to_string_pretty(&data).expect("serializing did not work :(");
-    println!("the string:");
-    println!("{}", &json_string);
-    println!("---------");
-
-    let data2: Data = from_str(&json_string).expect("deserializing did not work :(");
-
-    println!("{:?}", data2);
+    let log = log::Log::new();
+    log.to_file();
 }
